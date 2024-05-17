12b4c9e4fe171315f040bc1f93abb960e1d5b8bf
SLinkh <167535536+SLinkh@users.noreply.github.com>
2024-05-17 08:40:46 -0400

    Add files via upload

---
+++ input_box_button.py
@@ -0,0 +1,9 @@
+import pygame
+class Input_Box_Button:
+
+    def __init__(self):
+        self.x = x
+        self.y = y
+        self.image = pygame.image.load('Input New Law Case Image.png')
+        self.rect = pygame.Rect(self.x, self.y)
+    def collide_point
--- new_main.py
+++ new_main.py
@@ -3,6 +3,8 @@ from start_searching_button import Start_Searching_Button
 from input_box import TextInputBox
 import openpyxl
 import pandas as pd
+from input_box_button import Input_Box_Button
+from view_case_button import View_Case_Button
 
 clock = pygame.time.Clock()
 
@@ -24,6 +26,8 @@ my_font = pygame.font.SysFont('Arial', 15)
 title_font = pygame.font.SysFont('Arial', 35)
 pygame.display.set_caption("AP CSP Pygame!")
 s = Start_Searching_Button(135, 200)
+i = Input_Box_Button(135, 200)
+v = View_Case_Button(135, 220)
 spreadsheet_created = False
 
 # set up variables for the display
@@ -46,24 +50,31 @@ finish_input_case = False
 
 #Functions for the loop
 
+# Rectangle Collisions
+
+# def text_return_to_base():
+#     text = ""
+#     return text
+
+def case_name_rect_collision:
+
 def create_spreadsheet():
 
-def case_name():
-    case_input_rect = pygame.Rect(100, 200, 10, 70)
-    pygame.draw.rect(screen, color, case_input_rect, 2)
+
+def case_name(text):
     name_text_surface = my_font.render(text, True, (255, 255, 255))
     return name_text_surface and case_input_rect
 
 
 def input_data_in_spreadsheet():
 
-def case_ruling():
+def case_ruling(text):
     case_ruling_rect = pygame.Rect(100, 200, 10, 90)
     pygame.draw.rect(screen, color, case_ruling_rect, 2)
     ruling_text_surface = my_font.render(text, True, (255, 255, 255))
     return ruling_text_surface and case_ruling_rect
 
-def case_type():
+def case_type(text):
     case_type_rect = pygame.Rect(100, 200, 10, 110)
     pygame.draw.rect(screen, color, case_type_rect, 2)
     case_type_text_surface = my_font.render(text, True, (255, 255, 255))
@@ -77,15 +88,14 @@ def case_type():
         criminal_case_text_surface = my_font.render(text, True, (255, 255, 255))
     return case_type_rect and case_type_text_surface and civil_case_text_surface and civil_case_type or criminal_case_text_surface and criminal_case_type
 
-def ruling_court():
-    ruling_court_rect = pygame.Rect(100, 200, 10, 150)
-    pygame.draw.rect(screen, color, ruling_court_rect, 2)
+def ruling_court(text):
     ruling_court_text_surface = my_font.render(text, True, (255, 255, 255))
     return ruling_court_rect and ruling_court_text_surface
 
 
-def circuit_name():
-    circuit_name = pygame.Rect(100, 200, 10, 170)
+def circuit_name(text):
+    circuit_name_text_surface = my_font.render(text, True, (255, 255, 255))
+    return circuit_name_text_surface
 
 
 
@@ -102,7 +112,8 @@ while run:
             if s.rect.collidepoint(pos):
                 finish_input_case = False
                 end_title_click = True
-            if
+            if i.rect.collide_point(pos) and end_title_click:
+                input_screen = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             input_active = True
             text = ""
@@ -114,34 +125,36 @@ while run:
                 text = text[:-1]
             else:
                 text += event.unicode
-        text_surface = my_font.render(text, True, (255, 255, 255))
-        pygame.draw.rect(screen color, input_rect, 2)
 
-        if end_title_click:
+        if end_title_click and :
+            # Rectangle 1
+            case_input_rect = pygame.Rect(100, 200, 10, 70)
+            pygame.draw.rect(screen, color, case_input_rect, 2)
             if case_input_rect.collidepoint(pos):
-                case_name()
+                case_name(text)
+            # Rectangle 2
+            ruling_input_rect = pygame.Rect(200, 200, 100, 72)
+            pygame.draw.rect(screen, color, ruling_input_rect, 2)
+            if ruling_input_rect.collidepoint(pos):
+                case_ruling(text)
+            # Rectangle 3
+            case_type_rect = pygame.Rect(100, 200, 10, 110)
+            pygame.draw.rect(screen ,color, case_type_rect,2 )
+            if case_type_rect.collidepoint(pos):
+                case_type(text)
+            # Rectangle 4
+            ruling_court_rect = pygame.Rect(100, 200, 10, 150)
+            pygame.draw.rect(screen, color, ruling_court_rect, 2)
+            if ruling_court_rect.collidepoint(pos):
+                ruling_court(text)
+            # Rectangle 5
+            circuit_name_rect = pygame.Rect(100, 200, 10, 170)
+            pygame.draw.rect(screen, color, circuit_name_rect, 2)
+            if circuit_name_rect.collidepoint(pos):
+                circuit_name(text)
 
-            if number_of_enter == 0:
-                input_case_name = my_font.render("Case Name: ", True, (255, 255, 255))
-                case_input_rect = pygame.Rect(200, 200, 200, 72)
-            if number_of_enter == 1:
-                display_input_ruling = my_font.render("Plantiff or Defense", True, (255, 255, 255))
-                ruling_input_rect = pygame.Rect(200, 200, 100, 72)
-            if number_of_enter == 2:
-                display_input_case_type = my_font.render("Criminal or Civil", True, (255, 255, 255))
-                input_case_type = pygame.Rect(200, 200, 50, 72)
-            # Put an input box next to the display input case name, and do for the rest of the inputs
-            # once the number of cases ends, and the person hits enter, it goes back to the title screen
-            # If the case type that is in the input rectangle appears as Civil, then write that here.
-            if number_of_enter == 3 and input_case_type == "Civil":
-                    civil_case_type = input("What sector of law is this?")
-                    display_civil_case_typed = my_font.render(civil_case_type, True, (255, 255, 255))
-            if number_of_enter and input_case_type != "Civil"
 
-                if input_case_type != "Civil":
-                    display_civil_case_typed = my_font.render("", True, (255, 255, 255))
-            input_ruling_court = input("What type of court was this ruling in: Supreme, Circuit, or Court of Appeals?")
-            display_input_ruling_court = my_font.render(input_ruling_court, True, (255, 255, 255))
+
             if input_ruling_court == "Circuit" or "circuit" or "CIRCUIT":
                 circuit_name = input("Which Federal Circuit did this ruling take place?")
                 display_circuit_name = my_font.render(circuit_name, True, (255, 255, 255))
@@ -150,7 +163,7 @@ while run:
             finish_input_case = True
             data = [
                 ["Case Name", "Ruling", "Case Type", "Civil Case Type", "Ruling Court", "Circuit Name"],
-                [case_input_rect, input_ruling, input_case_type, civil_case_type, input_ruling_court, circuit_name]
+                [case_input_rect, input_ruling, input_case_type, input_ruling_court, circuit_name]
             ]
             for row in data:
                 sheet.append(row)
@@ -165,13 +178,28 @@ while run:
             run = False
 
     screen.fill((100, 100, 100))
+    if not input_screen:
+        screen.fill((100, 100, 100))
+        screen.blit(i.rect, i.image, (200, 200))
+        screen.blit(v.rect, v.image(200, 220))
+    if input_screen == True:
+        screen.fill((0, 0, 0))
+        screen.blit(name_text_surface, (200, 220))
+        screen.blit(ruling_text_surface, (200, 240))
+        screen.blit(case_type_text_surface, (200, 260))
+        screen.blit(case_type_rect, (200, 280))
+        if
     if not end_title_click:
         screen.blit(title_screen, (35, 75))
         screen.blit(s.image, s.rect)
 
     if end_title_click:
         screen.fill((0, 0, 0))
-        screen.blit(text_surface, (200, 200))
+        screen.blit(name_text_surface, (200, 220))
+        screen.blit(ruling_text_surface, (200, 240))
+        screen.blit(case_type_text_surface, (200, 260))
+        screen.blit(case_type_rect, (200, 280))
+        screen.blit()
         pygame.display.flip()
         screen.blit()
         # screen.fill((100, 100, 100))
